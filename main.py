ea294976423ba8e897ada9cf2fd331e8b73a1154
Pavel Dubinkin <pdubinkin@yandex.ru>
2023-03-19 20:07:36 +0300

    Залил фон и вставил картинку

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import pygame  # импортируем библиотеку
 
+BLUE = (175, 226, 207)  # Цвет фона в формате RGB
+
 window = pygame.display.set_mode((500, 400))  # Создаем окно 500 на 400
 pygame.display.set_caption('Симулятор жизни')  # Устанавливаем заголовок
+
+# Загружаем картинку
+cornmen_image = pygame.image.load("hero.png").convert_alpha()
+# Обозначаем форму и фиксируем координаты картинки на экране
+cornmen_rect = cornmen_image.get_rect(center=(250, 200))
+
+window.fill(BLUE)  # Заливаем фон
+
+# Размещаем картинку и форму в окне
+window.blit(cornmen_image, cornmen_rect)
+
+pygame.display.update()  # Обновляем окно
+
 game_over = False  # Переменная-флаг, меняется, когда нажмем крестик
 
 while game_over == False:  # Главный цикл программы
